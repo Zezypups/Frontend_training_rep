acb396563897aee3a19bbb601f4ddb3c381ad8ef
Zezypups <109433579+Zezypups@users.noreply.github.com>
2023-03-20 21:21:55 +0400

    Header is done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 import SearchBar from "./SearchBar";
 import Hello from "./Hello"
 import SubmitEmail from "./SubmitEmail"
+import Header from "./Header";
 
 
 function App() {
 
   return (
     <>
+      <Header />  
       <MainWrapper>
-      <SubmitEmail />
-      <SearchBar />
-      <Personal />
-      <Hello />
+        <SubmitEmail />
+        <SearchBar />
+        <Personal />
+        <Hello />
       </MainWrapper>
     </>
       
--- frontend/src/Personal.js
+++ frontend/src/Personal.js
@@ -2,7 +2,8 @@ import { Typography, Link } from "@material-ui/core";
 
 export default function Personal() {
   return (
-    <>
+    
+    <div id="Profile">
       <img src='Profile.jpg' alt='profile-img' />
       <Typography variant='h1'>
         Hello, I am Zezypups! I am a beginner front-end developer.
@@ -10,6 +11,6 @@ export default function Personal() {
       <Link href="https://github.com/Zezypups" target='blank'>
         Here is my GitHub
       </Link>
-    </>
+    </div>
   )
 }
