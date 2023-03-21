10bc849f072e60e69ba08307d307e518db180c6d
Zezypups <109433579+Zezypups@users.noreply.github.com>
2023-03-22 00:19:15 +0400

    Hello js is made

--- frontend/src/Hello.js
+++ frontend/src/Hello.js
@@ -1,23 +1,17 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
-import {Container, Alert} from "@mui/material"
-import Snackbar from '@mui/material/Snackbar';
 
 
 export default function Hello (props) {
-  
   const [data, setData] = useState(null);
-
   useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-      console.log(data)
-  })
+    axios.get('/api')
+      .then((response) => setData(response.data.message))
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <div>
       <p>{!data ? "Loading..." : data}</p>
     </div>
-  );
+  )
 };
-
