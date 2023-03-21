beb9683cdbf8cb10c73c672ed3bd335a56939eb5
Zezypups <109433579+Zezypups@users.noreply.github.com>
2023-03-21 23:21:23 +0400

    Trying to solve problems

--- frontend/src/Hello.js
+++ frontend/src/Hello.js
@@ -1,24 +1,23 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
+import {Container, Alert} from "@mui/material"
+import Snackbar from '@mui/material/Snackbar';
 
 
-export default function Hello () {
-const [hello, setHello] = useState([]);
+export default function Hello (props) {
+  
+  const [data, setData] = useState(null);
 
-useEffect(() =>{
-  axios
-  .get('/api')
-  .then(response => {
-    setHello(response.data);
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+      console.log(data)
   })
-  .catch(error => {
-    console.log(error);
-  });
-}, []);
-
-return (
-  <>
-  </>
-);
+  return (
+    <div>
+      <p>{!data ? "Loading..." : data}</p>
+    </div>
+  );
 };
 
