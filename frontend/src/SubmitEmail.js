f882c92616534f4fb46b800707b9d0b4130005eb
Zezypups <109433579+Zezypups@users.noreply.github.com>
2023-03-27 19:37:32 +0400

    Update SubmitEmail.js

--- frontend/src/SubmitEmail.js
+++ frontend/src/SubmitEmail.js
@@ -1,38 +1,83 @@
-/*export default function SubmitEmail() {
-  const addPosts = (title, body) => {
-    client
-       .post('/api/submit/:email', {
-          title: title,
-          body: body,
-       })
-       .then((response) => {
-          setPosts((posts) => [response.data, ...posts]);
-       });
- };
-}
-*/
+
 import axios from 'axios'
 import {useState} from 'react';
+import Snackbar from "@mui/material";
 
 
 export default function SubmitEmail() {
   const [email, setEmail] = useState(''); 
+  const [status, setStatus] = useState('');
+  const [err, setErr] = useState('');
+  const [open, setOpen] = useState(false);
   
   const handleInput = (event) => {
-      setEmail({...email, [event.target.name]: event.target.value})
+      setEmail(event.target.value)
   }
 
   function handleSubmit(event) {
     event.preventDefault()
-    axios.post(`/api/submit/${email}`, {email})
-    .then(response => console.log(response))
-    .catch(err => console.log(err))
+    axios
+        .post(`/api/submit/${email}`, {email})
+        .then(response => setStatus(response.data.message))
+        .catch(error => setErr(error))
   }
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const action = (
+    <div>
+        {status ? 
+                    <Alert 
+                        variant="filled" 
+                        severity="success" 
+                        onClose={handleClose}
+                        sx={{ width: '100%' }}
+                    >
+                        {status}
+                    </Alert>
+                : 
+                    <Alert 
+                        variant="filled" 
+                        severity="warning" 
+                        onClose={handleClose}
+                        sx={{ width: '100%' }}
+                    >
+                        {err}
+                    </Alert>}
+    </div>);
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        Email: <input type="text" name="email" onChange={handleInput}></input>
-        <button className="submit-button">Submit</button>
+        Email: <input 
+                type="email" 
+                name="email" 
+                value={email} 
+                onChange={handleInput}>
+                </input>
+        <button 
+                type="submit"
+                className="submit-button" onClick={handleClick}>
+                  Submit
+        </button>
+        <Snackbar 
+                open={open} 
+                autoHideDuration={3000} 
+                anchorOrigin={{ vertical:'top', horizontal: 'left',  }}
+                action={action}
+                onClose={handleClose}
+                > 
+        </Snackbar>
       </form>
     </div>
   )
